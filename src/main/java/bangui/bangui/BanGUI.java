af85f127478e5048ccc38f0378aa083ae7c97e27
charlie-ward <59178341+Charlie-Ward@users.noreply.github.com>
2022-08-30 11:43:14 +0100

    Added GUI and ban functionality

--- src/main/java/bangui/bangui/BanGUI.java
+++ src/main/java/bangui/bangui/BanGUI.java
@@ -1,12 +1,24 @@
 package bangui.bangui;
 
+import bangui.bangui.commands.BanGUICommand;
+import bangui.bangui.commands.HelpCommand;
+import bangui.bangui.listeners.BanInventoryListener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class BanGUI extends JavaPlugin {
 
+    private static BanGUI plugin;
+
     @Override
     public void onEnable() {
+
+        plugin = this;
+
         // Plugin startup logic
+        getCommand("bangui").setExecutor(new BanGUICommand());
+        getCommand("BGhelp").setExecutor(new HelpCommand());
+
+        getServer().getPluginManager().registerEvents(new BanInventoryListener(), this);
 
     }
 
@@ -14,4 +26,9 @@ public final class BanGUI extends JavaPlugin {
     public void onDisable() {
         // Plugin shutdown logic
     }
+
+    public static BanGUI getPlugin(){
+        return plugin;
+    }
+
 }
---
+++ src/main/java/bangui/bangui/commands/HelpCommand.java
@@ -0,0 +1,40 @@
+package bangui.bangui.commands;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class HelpCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        if (sender instanceof Player){
+            Player p = (Player) sender;
+
+           if (p.isOp() == true){
+
+               p.sendMessage(ChatColor.GOLD + "BanGUI Plugin Help");
+
+               p.sendMessage(ChatColor.RED + "Commands");
+
+               p.sendMessage(ChatColor.WHITE + "/bangui");
+               p.sendMessage(ChatColor.GRAY + "Opens the Ban GUI");
+               p.sendMessage(ChatColor.WHITE + "/help");
+               p.sendMessage(ChatColor.GRAY + "Shows all available commands");
+
+           }else{
+
+                p.sendMessage(ChatColor.GOLD + "BanGUI Plugin Help");
+                p.sendMessage(ChatColor.RED + "There are currently no commands for un-opped players");
+
+            }
+
+        }
+
+        return true;
+    }
+
+}
